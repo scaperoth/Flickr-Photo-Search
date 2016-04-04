25d887fa170f0b4d9f3dc2b789c78e11440daa31
scaperoth <mscapero@gwu.edu>
2016-04-03 20:10:52 -0400

    added flickr api

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -1,13 +1,15 @@
 module SearchHelper
   def self.searchByTags(tags)
 
-   flickrAPI += "https://api.flickr.com/services/rest/?"
-flickrAPI += "&method=flickr.photos.search"
-flickrAPI += "&api_key=9012151640d5486e63780579ff3b9cae"
-flickrAPI += "&tags=#{tags}"
-flickrAPI += "&per_page=5"
-flickrAPI += "&page=1"
-flickrAPI += "&format=json"
+    flickrAPI += "https://api.flickr.com/services/rest/?"
+    flickrAPI += "&method=flickr.photos.search"
+    flickrAPI += "&api_key=#{FLICKR_API_}"
+    flickrAPI += "&tags=#{tags}"
+    flickrAPI += "&per_page=5"
+    flickrAPI += "&page=1"
+    flickrAPI += "&format=json"
+    
+    photos = get_request(flickrAPI)
 
   end
 
@@ -23,15 +25,15 @@ flickrAPI += "&format=json"
     http = Net::HTTP.new(uri.host, uri.port)
 
     request = Net::HTTP::Post.new(uri.request_uri)
-    
+
     #request.set_form_data({"user[name]" => "testusername", "user[email]" => "testemail@yahoo.com"})
 
     response = http.request(request)
-    
+
     formatted_response = JSON.parse(response.body)
-    
+
     formatted_response
-    
+
   end
 
   def searchByTags(tags)
